b43ec7d0e9cc493c413bcde59a2e20f04b748cde
Tomasz <abroadchunk@gazeta.pl>
2017-05-09 10:42:21 +0200

    Utworzenie Klasy TetrisPanel Utworzenie klasy jako graficzna reprezentacja klasy Plane.

--- src/pl/tetris/Main.java
+++ src/pl/tetris/Main.java
@@ -3,11 +3,18 @@ package pl.tetris;
 import pl.tetris.blocks.Block;
 import pl.tetris.blocks.Square;
 import pl.tetris.blocks.SquareBlock;
+import pl.tetris.gui2.TetrisPanel;
 import pl.tetris.plane.Plane;
+
+import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new TetrisPanel(30,70));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
