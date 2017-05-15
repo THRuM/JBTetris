3df57f595bb2b5233c657b15d8c9f019b36319ca
Awaltir <awaltir07@gmail.com>
2017-05-15 16:53:06 +0200

    Stworzenie GuiUpdator Klasa ma na celu akutalizowanie informacji o grze w gui a także posiada metode która pozwala jej na opóźnianie tego aby animacja zdąrzyła się wykonać

---
+++ src/pl/tetris/gui/GuiUpdator.java
@@ -0,0 +1,32 @@
+package pl.tetris.gui;
+
+
+import pl.tetris.game.TGame;
+
+public class GuiUpdator extends Thread {
+
+    private TetrisPanel tetrisPanel;
+    private HeadPanel headPanel;
+    private TGame tGame;
+
+    public GuiUpdator(TetrisPanel tetrisPanel, HeadPanel headPanel, TGame tGame) {
+        super("GuiUpdator");
+        this.tetrisPanel = tetrisPanel;
+        this.headPanel = headPanel;
+        this.tGame = tGame;
+        start();
+    }
+
+    @Override
+    public void run() {
+        do{
+            tetrisPanel.refillSquares();
+            headPanel.updateScore();
+            try {
+                Thread.sleep(33);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } while (tGame.isGameRunning());
+    }
+}
