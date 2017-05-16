6688ca9bbb59e975c3aba2b786c4ebb86dc59b5e
Tomasz <abroadchunk@gazeta.pl>
2017-05-16 11:14:09 +0200

    Aktualizacja klasy TetrisPanel Dodanie metody do ponownego rysowania planszy. Dodany konwerter do enuma zamieniający nasze enumy Squera na klasę Color

--- src/pl/tetris/blocks/Square.java
+++ src/pl/tetris/blocks/Square.java
@@ -1,6 +1,19 @@
 package pl.tetris.blocks;
 
+import java.awt.*;
+
 /* Możliwe stany planszy */
 public enum Square {
-    BLANK, GREEN, RED, BLUE
+    BLANK, GREEN, RED, BLUE;
+
+    public static Color squareToColor(Square square){
+        switch (square){
+            case RED: return new Color(255,0,0);
+            case BLUE: return new Color(17,0,255);
+            case GREEN: return new Color(24,178,75);
+            case BLANK: return new Color(255,255,255);
+        }
+
+        return new Color(255,255,255);
+    }
 }
--- src/pl/tetris/gui/TetrisPanel.java
+++ src/pl/tetris/gui/TetrisPanel.java
@@ -1,6 +1,12 @@
 package pl.tetris.gui;
 
+import pl.tetris.blocks.Square;
+import pl.tetris.plane.Plane;
+
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.border.SoftBevelBorder;
 import java.awt.*;
 
 /**
@@ -10,6 +16,7 @@ public class TetrisPanel extends JPanel {
     private int width;
     private int height;
     private JButton squares[][];
+    private Plane plane;
 
     public TetrisPanel(int width, int height){
         super(new GridLayout(height, width));
@@ -17,8 +24,25 @@ public class TetrisPanel extends JPanel {
         this.width = width;
         this.height = height;
         squares = new JButton[height][width];
-
         fillSquares();
+    }
+
+    public void setPlane(Plane plane){
+        this.plane = plane;
+    }
+
+    public void refillSquares(){
+        Square squareArray[][] = plane.getPlane();
+        Square singleSqare;
+        System.out.println("Wątek TetrisPanel");
+        for (int i = 0; i < squares.length; i++) {
+            for (int j = 0; j < squares[i].length; j++) {
+                singleSqare = squareArray[i][j];
+                squares[i][j].setBackground(Square.squareToColor(singleSqare));
+                if(singleSqare != Square.BLANK)
+                    squares[i][j].setBorder(new SoftBevelBorder(1));
+            }
+        }
     }
 
     private void fillSquares(){
@@ -28,10 +52,11 @@ public class TetrisPanel extends JPanel {
         for(int i=0; i < squares.length; i++){
             for(int j=0; j < squares[i].length; j++){
                 JButton button = new JButton();
-                button.setBackground(new Color(255,255,255));
+                button.setBackground(Square.squareToColor(Square.BLANK));
                 button.setSize(squareWidth, squareHeight);
                 button.setMargin(new Insets(0,0,0,0));
                 button.setEnabled(false);
+                button.setBorder(new SoftBevelBorder(1));
 
                 squares[i][j] = button;
                 super.add(button);
