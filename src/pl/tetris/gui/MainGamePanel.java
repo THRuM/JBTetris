0a9b36211c8f91a9019a5fae582c510fb6125f4b
Łukasz <markiewicz.lukasz@hotmail.com>
2017-05-16 12:17:02 +0200

    Dodanie MainGamePanel
    Dodanie panelu oraz poprawki w TFrame

---
+++ src/pl/tetris/gui/MainGamePanel.java
@@ -0,0 +1,30 @@
+package pl.tetris.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Łukasz on 15.05.2017.
+ */
+public class MainGamePanel extends JPanel{
+
+    TetrisPanel tetrisPanel;
+    HeadPanel headPanel;
+    TGame tGame;
+
+    public MainGamePanel(TGame tGame){
+        super(new BorderLayout());
+        this.tGame = tGame;
+        tetrisPanel = new TetrisPanel(tGame.getPlaneSize()[0],tGame.getPlaneSize()[1]);
+        tetrisPanel.setPlane(tGame.getPlane());
+
+        headPanel = new HeadPanel(tGame);
+
+        add(tetrisPanel, BorderLayout.CENTER);
+        add(headPanel, BorderLayout.PAGE_START);
+
+
+        new GuiUpdator(tetrisPanel, headPanel, tGame);
+        this.tGame.start();
+    }
+}
